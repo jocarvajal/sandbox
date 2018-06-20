308654dd6359788230742cba637cd9a2db2fe202
tammypmc <tammymoscoso2@gmail.com>
2018-06-20 17:43:31 -0600

    Agregar documentacion y mejorar algoritmo

--- prueba-palindromo.py
+++ prueba-palindromo.py
@@ -1,8 +1,23 @@
 def palindromo(texto):
-    texto = texto.lower()
-    texto = texto.replace(" ", "")
-    if str(texto) == str(texto)[::-1]:
-        print ("es palindromo")
-    else:
-        print ("no es")
+	""" 
+	elimina caracteres no deseados, compara el texto orignal con el volteado
+	"""
+	texto = texto.lower()
+	texto = texto.replace(" ", "")
+	if str(texto) == str(texto)[::-1]:
+	        print ("Es palindromo")
+	else:
+		print ("No es")
 
+def main():
+	"""
+	ciclo infinito hasta que el usuario quiera cerrar el programa
+	"""	
+	while True:
+    		texto = raw_input("Ingrese su palabra ")
+    		palindromo(texto)
+    		flag = raw_input("Desea ingresar otra palabra? S/N ")
+		if flag == "N":
+			break
+
+main()
